9dc3cc6967b41f9f3ce2e2a5ac065816d7e575dd
AlexJuma-CCMB <jumalex99@gmail.com>
2025-11-14 13:04:56 -0600

    Meet the team cube added to landing --- style inconsistencies present

--- ats_django/ats_system/urls.py
+++ ats_django/ats_system/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os  # <-- Add this import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # --- Add this line to serve the team_pics folder ---
+    urlpatterns += static('/team_pics/', document_root=os.path.join(settings.BASE_DIR, 'team_pics'))
